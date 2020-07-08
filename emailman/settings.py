1eb90528a056d261d5f26c74d4c8600f8e6957e6
Parth Pandya <34797335+parthpandyappp@users.noreply.github.com>
2020-07-08 15:21:03 +0530

    Update settings.py

--- emailman/settings.py
+++ emailman/settings.py
@@ -120,9 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, '/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 # Email settings
 EMAIL_HOST = 'smtp.gmail.com'
